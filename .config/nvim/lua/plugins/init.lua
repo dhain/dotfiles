c05d8fed35d61b56c8aa92bd9903b0c44e96908c
Sadie Hain <sadie@zognot.org>
2024-10-18 10:33:48 -0400

    Add nvim-treesitter

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,3 +1,14 @@
 return {
     { "wincent/command-t", main = "wincent.commandt", opts = {} },
+    { "nvim-treesitter/nvim-treesitter", build= ":TSUpdate", opts = {
+	ensure_installed = {
+		"c", "lua", "python", "javascript", "markdown", "bash", "css",
+		"git_config", "git_rebase", "gitcommit", "gitignore", "go",
+		"html", "jq", "json", "make", "markdown_inline", "nginx",
+		"perl", "sql", "ssh_config", "terraform", "yaml"
+	},
+	sync_install = false,
+	auto_install = true,
+	highlight = { enable = true }
+    } },
 }
