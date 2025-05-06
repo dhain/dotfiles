fb6b6d054445a6816fa9b39ed2c3639daf0c8fd1
Sadie Hain <sadie@zognot.org>
2025-05-06 16:08:10 -0400

    Use different path for Obsidian.nvim by OS

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -5,33 +5,41 @@ return {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
   },
-  opts = {
-    workspaces = {
-      {
-        name = "Garden",
-        path = "~/mnt/borf/Library/Mobile Documents/iCloud~md~obsidian/Documents/Garden",
+  config = function()
+    local path = ""
+    if vim.loop.os_uname().sysname == "Darwin" then
+      path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Garden"
+    else
+      path = "~/mnt/borf/Library/Mobile Documents/iCloud~md~obsidian/Documents/Garden"
+    end
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "Garden",
+          path = path,
+        },
       },
-    },
-    templates = {
-      folder = "Templates",
-      substitutions = {
-        ["date:dddd, MMMM DD YYYY"] = function()
-          return os.date("%A, %B %d %Y")
-        end
+      templates = {
+        folder = "Templates",
+        substitutions = {
+          ["date:dddd, MMMM DD YYYY"] = function()
+            return os.date("%A, %B %d %Y")
+          end
+        },
       },
-    },
-    daily_notes = {
-      template = "Daily Note.md",
-    },
-    attachments = {
-      img_folder = "Attachments",
-    },
-    note_path_func = function(spec)
-      local path = spec.dir / tostring(spec.title)
-      return path:with_suffix(".md")
-    end,
-    wiki_link_func = "use_alias_only",
-    disable_frontmatter = true,
-    follow_url_func = vim.ui.open,
-  },
+      daily_notes = {
+        template = "Daily Note.md",
+      },
+      attachments = {
+        img_folder = "Attachments",
+      },
+      note_path_func = function(spec)
+        local path = spec.dir / tostring(spec.title)
+        return path:with_suffix(".md")
+      end,
+      wiki_link_func = "use_alias_only",
+      disable_frontmatter = true,
+      follow_url_func = vim.ui.open,
+    })
+  end
 }
