2d0227f4909a50d44c0e7f73dc91eaff0705323a
Sadie Hain <sadie@zognot.org>
2025-05-07 11:25:11 -0400

    Fix

--- .local/bin/tmux-either.sh
+++ .local/bin/tmux-either.sh
@@ -4,6 +4,7 @@ if [ -e /usr/bin/tmux ]; then
     TMUX=/usr/bin/tmux
 elif [ -e /usr/local/bin/tmux ]; then
     TMUX=/usr/local/bin/tmux
+    export PATH="/usr/local/bin:$PATH"
 elif [ -e /opt/homebrew/bin/tmux ]; then
     TMUX=/opt/homebrew/bin/tmux
     export PATH="/opt/homebrew/bin:$PATH"
