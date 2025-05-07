491676fd5b38035d85e7bb460f5f8bb5c534051c
Sadie Hain <sadie@zognot.org>
2025-05-07 11:12:16 -0400

    Add vim-tmux-navigator

--- .config/nvim/lua/plugins/llm.lua
+++ .config/nvim/lua/plugins/llm.lua
@@ -6,8 +6,11 @@ return {
     lsp = {
       bin_path = vim.api.nvim_call_function("stdpath", { "data" }) .. "/mason/bin/llm-ls",
     },
+    -- enable_suggestions_on_startup = false,
+    enable_suggestions_on_files = { "*.lua", "*.py" },
 
     model = "codellama:code",
+    -- model = "codellama:python",
     tokens_to_clear = { "<EOT>" },
     fim = {
       enabled = true,
@@ -18,6 +21,7 @@ return {
     context_window = 4096,
     tokenizer = {
       repository = "codellama/CodeLlama-7b-hf",
+      -- repository = "codellama/CodeLlama-7b-Python-hf",
     },
 
     -- model = "qwen2.5-coder:1.5b",
@@ -31,6 +35,19 @@ return {
     -- context_window = 32768,
     -- tokenizer = {
     --   repository = "Qwen/Qwen2.5-Coder-1.5B",
+    -- },
+
+    -- model = "codegemma:2b-code",
+    -- tokens_to_clear = { "<|file_separator|>" },
+    -- fim = {
+    --   enabled = true,
+    --   prefix = "<|fim_prefix|>",
+    --   middle = "<|fim_middle|>",
+    --   suffix = "<|fim_suffix|>",
+    -- },
+    -- context_window = 4096,
+    -- tokenizer = {
+    --   repository = "google/codegemma-2b",
     -- },
   }
 }
