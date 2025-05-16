bbc6bbbd5e77ba5cd4b686de27a7d369b94b6093
Sadie Hain <sadie@zognot.org>
2025-05-16 09:44:35 -0400

    Better wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,12 +1,22 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local is_darwin = function()
+  return wezterm.target_triple:find("darwin") ~= nil
+end
+
+config.initial_cols = 150
+config.initial_rows = 60
 
 config.font = wezterm.font("MesloLGL Nerd Font Mono")
-config.font_size = 14
+config.font_size = is_darwin() and 14 or 10
 config.color_scheme = 'Catppuccin Mocha'
+config.colors = {
+  background = "black"
+}
 
 config.enable_tab_bar = false
-config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
+
+config.default_prog = { os.getenv("HOME") .. "/.local/bin/tmux-either.sh" }
 
 return config
