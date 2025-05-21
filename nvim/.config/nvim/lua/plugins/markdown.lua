97fdf5087e29289d0649dc123c4cb083ab4e390b
Sadie Hain <sadie@zognot.org>
2025-05-21 17:01:01 -0400

    Add markdown preview

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,4 +2,13 @@ return {
   {
     "bullets-vim/bullets.vim",
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
