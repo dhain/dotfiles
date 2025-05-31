a582a9510fb81e31e56116ca3338fd10a7639030
Sadie Hain <sadie@zognot.org>
2025-05-31 10:08:53 -0400

    Open help like a regular buffer

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,16 +32,31 @@ vim.o.cursorline = true
 
 vim.g.bullets_checkbox_markers = " ◐●✓"
 
-vim.keymap.set('n', '<cr>', ":nohlsearch<cr>", {silent = true})
-vim.keymap.set('n', '<leader><leader>', "<c-^>")
-vim.keymap.set('n', '<c-h>', "<c-w>h")
-vim.keymap.set('n', '<c-j>', "<c-w>j")
-vim.keymap.set('n', '<c-k>', "<c-w>k")
-vim.keymap.set('n', '<c-l>', "<c-w>l")
+local n_opts = { silent = true, noremap = true }
+local t_opts = { silent = true }
+vim.keymap.set('n', '<cr>', ":nohlsearch<cr>", n_opts)
+vim.keymap.set('n', '<leader><leader>', "<c-^>", n_opts)
+vim.keymap.set('n', '<c-h>', "<c-w>h", n_opts)
+vim.keymap.set('n', '<c-j>', "<c-w>j", n_opts)
+vim.keymap.set('n', '<c-k>', "<c-w>k", n_opts)
+vim.keymap.set('n', '<c-l>', "<c-w>l", n_opts)
+vim.keymap.set('t', '<c-h>', "<c-\\><c-n><c-w>h", t_opts)
+vim.keymap.set('t', '<c-j>', "<c-\\><c-n><c-w>j", t_opts)
+vim.keymap.set('t', '<c-k>', "<c-\\><c-n><c-w>k", t_opts)
+vim.keymap.set('t', '<c-l>', "<c-\\><c-n><c-w>l", t_opts)
 vim.keymap.set('v', '<leader>y', '"*y')
 
 vim.api.nvim_create_autocmd("BufLeave", { pattern = "*", command = "let b:winview = winsaveview()" })
 vim.api.nvim_create_autocmd("BufEnter", { pattern = "*", command = "if(exists('b:winview')) | call winrestview(b:winview) | endif" })
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function(ev)
+    if vim.bo[ev.buf].filetype == 'help' then
+      vim.cmd.only()
+      vim.bo[ev.buf].buflisted = true
+    end
+  end,
+})
 
 require("lazy-bootstrap")
 require("lazy").setup("plugins")
